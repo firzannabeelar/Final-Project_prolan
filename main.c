eb1a3656cd88d00a106f701c5ac6335f1e658c5f
Firzan <145427772+firzannabeelar@users.noreply.github.com>
2023-12-16 16:15:35 +0700

    rev. menu & inlude str-file

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "desa.c"
 #include "sort.c"
+#include "operasiString.c"
+#include "operasiFile.c"
 
 int main() {
     printf("\n");
@@ -10,10 +12,13 @@ int main() {
     printf("Dirancang oleh : \n");
     printf("Firzannabeel Aqila Rafid (22081010285)\nBenaya Titus Natanael (22081010302)\nBayu Setia Aji (22081010294)\n");
     printf("\n");
+
+    int pil;
     printf("Pilihan menu : \n");
     printf("1. Struct Data Desa\n");
-    printf("2. Sorting Data Desa (Jumlah Tanggungan)\n");
-    int pil;
+    printf("2. Sorting Data Desa Berdasarkan Jumlah Tanggungan\n");
+    printf("3. Operasi String\n");
+    printf("4. Operasi File\n");
     printf("Pilih : ");
     scanf("%d", &pil);
 
@@ -23,10 +28,16 @@ int main() {
     else if(pil == 2){
         displaysort();
     }
-    else{
-            printf("Pilihan salah.\n");
-            return 1;
+    else if(pil == 3){
+        displayString();
     }
+    else if(pil == 4){
+        displayfile();
+    }   
+    else{ 
+        printf("Program Selesai");
+    }
+    
 
     return 0;
 }
